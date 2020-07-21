9c9ca7f4c531c4cee24774166f1fbd9bb8d55a8b
Sathyanarayan Sudarshan <ssudarshan7@gatech.edu>
2020-07-21 19:41:48 +0530

    Minor Fixes
    Just some small edits to make it more pro. Nothing Major. 👍

--- Git-Repos-Init.py
+++ Git-Repos-Init.py
@@ -6,7 +6,7 @@ def os_select():
     os.system("clear")
     print("\t\t\t\t OS SELECTION \n")
     print("Supported OS: \n 1.Windows \n 2.Linux-Debian \n")
-    os_name = int(input("Enter the choice: "))
+    os_name = int(input("Enter OS choice: "))
     return os_name
 
 
@@ -16,22 +16,28 @@ prj_name = str(input("Project Name: "))
 file_statement = "mkdir " + prj_path + prj_name
 init_statement = "cd " + prj_path + prj_name + " && git init"
 print("Are you going to push this to Github? (Y|N): ")
-opt = str(input())
-str.lower(opt)
-if opt == 'y':
-    push_code = str(input("PUSH CODE: "))
-    repos = str(input("Remote Repos Name: "))
-    selection = os_select()
-elif opt =='n':
-    selection = os_select()
-else:
-   os.system("clear")
-   print("ERORR")
+opt = (input()).lower()
+
+try:
+    if (opt == 'y') :
+        push_code = str(input("PUSH CODE: "))
+        repos = input("Remote Repos Name: ")
+        selection = os_select()
+    elif (opt =='n') :
+        selection = os_select()
+
+    else:
+        os.system("clear")
+        os_err = ValueError()
+        os_err.strerror("Only options y and n exist.")
+        raise os_err
+except:
+    print("An unknown error occured. ")
 
 
 #Git Push
 def push(statement):
-    os.system("cd "+prj_path + prj_name + " && git remote add " +repos+" " + statement + " && git push -u "+repos+ " master")
+    os.system("cd "+prj_path + prj_name + " && git remote add " +repos+" " + statement + " && git push -u "+ repos + " master")
 
     
     
@@ -39,7 +45,7 @@ def push(statement):
 
 
 if selection == 1:
-    print("windows")
+    print("Windows")
 
 elif selection == 2:
     #folder create
@@ -49,13 +55,13 @@ elif selection == 2:
     #git initialise
     os.system(init_statement)
     #file create
-    c_opt = str(input("Do you want to create a file (Y|N): "))
-    if str.lower(c_opt) == 'y':
-     while c_opt =='y':   
-        os.system("clear")
-        file_name = str(input("File name: "))
-        os.system("touch " + prj_path + prj_name +"/" + file_name) #create a empty file
-        c_opt = str(input("\n Do you want to create one more file? (Y|N): "))
+    c_opt = input("Do you want to create a file (Y|N): ").lower()
+    if (c_opt == 'y') :
+        while c_opt =='y':   
+            os.system("clear")
+            file_name = input("File name: ")
+            os.system("touch " + prj_path + prj_name +"/" + file_name) #create a empty file
+            c_opt = input("\n Do you want to create one more file? (Y|N): ").lower()
 
     e = 'y'
     git_status = "clear" + "&& cd " + prj_path + prj_name +"&& git status"
@@ -63,9 +69,9 @@ elif selection == 2:
     git_log = "cd " + prj_path +prj_name +" && git log"
     os.system(git_status) # list the files
     while e == 'y':
-        info = str(input("Enter the file name to add: "))
-        os.system("cd " + prj_path + prj_name + "&& git add " + info)
-        e = str(input("Do u want to add one more file (Y|N): "))
+        file_name = input("Enter the file name to add: ")
+        os.system("cd " + prj_path + prj_name + "&& git add " + file_name)
+        e = input("Do u want to add one more file (Y|N): ")
           
     #Push to remote
     if opt == 'y':
@@ -81,7 +87,7 @@ elif selection == 2:
         os.system(git_status)
         os.system(git_log)
 
-        print("\n\n Pushing the repos\n\n")
+        print("\n\n Pushing the repositories\n\n")
         
         push(push_code)
         
@@ -89,13 +95,13 @@ elif selection == 2:
         os.system("clear")
         os.system(git_status)
         print("Project has been created and files as beed added")  
-        o = input("Do you want to commit it now(Y|N): ")
-        str.lower(o)
+        o = input("Do you want to commit  now(Y|N): ").lower()
+        
 
         #git commit 
 
         if o == 'y':
-            commit_msg = input("Enter the commit mssg: ")
+            commit_msg = input("Enter the commit message: ")
             os.system(git_commit +'"'+commit_msg +'"') 
             print(git_commit +'"'+commit_msg +'"')
             os.system(git_status)
@@ -103,7 +109,6 @@ elif selection == 2:
 
 
 
-          
 else:
     print("Error")
 
